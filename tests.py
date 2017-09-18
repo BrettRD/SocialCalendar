539e115f389ac34fc6c20ff01d4349b9da4f3b08
BrettRD <brett.downing1@gmail.com>
2017-09-19 04:45:54 +0800

    extends test cases to cover the updaters

--- tests.py
+++ tests.py
@@ -1,20 +1,36 @@
 from socialCalendarMinimal import *
-engine = create_engine('sqlite+pysqlcipher:///:memory:', echo=True)
+engine = create_engine('sqlite:///:memory:', echo=True)
 Base.metadata.create_all(engine)
-frank = Person(name='Frank')
 from sqlalchemy.orm import sessionmaker
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+frank = Person(name='Frank')
 session.add(frank)
 session.commit()
-kino = Encounter(person=frank, startTime=datetime.now())
+
+terminalMenu = Updater(name="terminal terminalMenu")
+session.add(terminalMenu)
+session.commit()
+
+franksAddress=Addresses(person=frank, updater=terminalMenu, handle="Frank's face")
+session.add(franksAddress)
+session.commit()
+
+frank.addresses
+terminalMenu.addresses
+
+kino = Encounter(person=frank, updater=terminalMenu, startTime=datetime.now(), brief="cinema")
 session.add(kino)
 session.commit()
+
 frank.encounters
-markt=Encounter(person=frank, startTime=datetime.now(), notes="some text")
+
+markt=Encounter(person=frank, updater=terminalMenu, startTime=datetime.now(), notes="some text")
 session.add(markt)
 session.commit()
+
 frank.encounters
 franksBirthday=ImportantDates(person=frank, startDate=datetime.now(), brief="Birthday")
 session.add(franksBirthday)
